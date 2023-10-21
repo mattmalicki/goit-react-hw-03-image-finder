1d3a98c3ca44dda3f0f756a3eae8c2fd777504a7
mattmalicki <mateusz.malicki96@gmail.com>
2023-10-21 19:10:09 +0200

    Added propTypes to SearchBar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import styles from './Searchbar.module.css';
 
 export class Searchbar extends Component {
@@ -23,3 +24,7 @@ export class Searchbar extends Component {
     );
   }
 }
+
+Searchbar.propTypes = {
+  submit: PropTypes.func,
+};
